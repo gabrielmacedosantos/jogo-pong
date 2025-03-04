f0a04220d91381e42371ab2eb6f9e5b86dd67f1f
Gabriel Macedo <99225975+gabrielmacedosantos@users.noreply.github.com>
2025-03-04 00:43:49 -0300

    Update pong.js

--- pong.js
+++ pong.js
@@ -5,14 +5,12 @@ let alturaBarra;
 let velocidadeJogador = 5;
 let velocidadeComputadorBase = 2;
 let suavizacao = 0.2;
-let fundoImg;
-let imgBola;
-let placarJogador = 0;
-let placarComputador = 0;
+let fundoImg, imgBola;
+let placarJogador = 0, placarComputador = 0;
 let botoesCriados = false;
 
 function definirTamanhos() {
-    larguraRaquete = width * 0.01;
+    larguraRaquete = width * 0.02;
     alturaRaquete = height * 0.2;
     alturaBarra = height * 0.02;
 }
@@ -23,61 +21,64 @@ function preload() {
 }
 
 function setup() {
-    let canvas = createCanvas(900, 450);
-    canvas.position((windowWidth - width) / 2, (windowHeight - height) / 2);
+    createCanvas(windowWidth, windowHeight * 0.8).position(0, (windowHeight - height) / 2);
     definirTamanhos();
-    jogadorY = height / 2 - alturaRaquete / 2;
-    computadorY = height / 2 - alturaRaquete / 2;
-    alvoJogadorY = jogadorY;
-    alvoComputadorY = computadorY;
-    reiniciarBola();
-    
-    if (windowWidth < 600) criarBotoes();
+    reiniciarJogo();
+    criarBotoes();
 }
 
 function draw() {
     background(fundoImg);
-    
+    desenharPlacar();
+    desenharElementos();
+    atualizarBola();
+    movimentarJogador();
+    movimentarComputador();
+}
+
+function desenharPlacar() {
     fill(255);
-    textSize(32);
-    textAlign(CENTER);
-    text(`${placarJogador} - ${placarComputador}`, width / 2, 50);
-    
+    textSize(width * 0.05);
+    textAlign(CENTER, CENTER);
+    text(`${placarJogador} - ${placarComputador}`, width / 2, height * 0.1);
+}
+
+function desenharElementos() {
     fill(255);
     rect(20, jogadorY, larguraRaquete, alturaRaquete);
     rect(width - 30, computadorY, larguraRaquete, alturaRaquete);
-    
-    let tamanhoBola = width * 0.03;
-    image(imgBola, bolaX - tamanhoBola / 2, bolaY - tamanhoBola / 2, tamanhoBola, tamanhoBola);
-    
+    image(imgBola, bolaX - width * 0.015, bolaY - width * 0.015, width * 0.03, width * 0.03);
+}
+
+function atualizarBola() {
     bolaX += velocidadeBolaX;
     bolaY += velocidadeBolaY;
-    
     if (bolaY < alturaBarra || bolaY > height - alturaBarra) velocidadeBolaY *= -1;
-    
-    if (bolaX < 30 && bolaY > jogadorY && bolaY < jogadorY + alturaRaquete) {
-        velocidadeBolaX *= -1;
-        modificarVelocidadeBola();
-    }
-    
-    if (bolaX > width - 30 && bolaY > computadorY && bolaY < computadorY + alturaRaquete) {
-        velocidadeBolaX *= -1;
-        modificarVelocidadeBola();
-    }
-    
-    if (bolaX < 0) { placarComputador++; reiniciarBola(); }
-    if (bolaX > width) { placarJogador++; reiniciarBola(); }
-    
+    if (colisaoRaquete(jogadorY, 30)) velocidadeBolaX *= -1, modificarVelocidadeBola();
+    if (colisaoRaquete(computadorY, width - 30)) velocidadeBolaX *= -1, modificarVelocidadeBola();
+    if (bolaX < 0) placarComputador++, reiniciarBola();
+    if (bolaX > width) placarJogador++, reiniciarBola();
+}
+
+function colisaoRaquete(posicaoY, limiteX) {
+    return bolaX < limiteX + larguraRaquete && bolaX > limiteX - larguraRaquete && bolaY > posicaoY && bolaY < posicaoY + alturaRaquete;
+}
+
+function movimentarJogador() {
     if (keyIsDown(87)) alvoJogadorY -= velocidadeJogador;
     if (keyIsDown(83)) alvoJogadorY += velocidadeJogador;
-    
     alvoJogadorY = constrain(alvoJogadorY, alturaBarra, height - alturaRaquete - alturaBarra);
     jogadorY = lerp(jogadorY, alvoJogadorY, suavizacao);
-    
-    velocidadeComputadorBase + abs(velocidadeBolaX) * 0.1;
-    alvoComputadorY = bolaY - alturaRaquete / 2;
-    alvoComputadorY = constrain(alvoComputadorY, alturaBarra, height - alturaRaquete - alturaBarra);
+}
+
+function movimentarComputador() {
+    alvoComputadorY = constrain(bolaY - alturaRaquete / 2, alturaBarra, height - alturaRaquete - alturaBarra);
     computadorY = lerp(computadorY, alvoComputadorY, suavizacao);
+}
+
+function reiniciarJogo() {
+    jogadorY = computadorY = alvoJogadorY = alvoComputadorY = height / 2 - alturaRaquete / 2;
+    reiniciarBola();
 }
 
 function reiniciarBola() {
@@ -88,23 +89,20 @@ function reiniciarBola() {
 }
 
 function modificarVelocidadeBola() {
-    let direcaoX = velocidadeBolaX > 0 ? 1 : -1;
-    let direcaoY = velocidadeBolaY > 0 ? 1 : -1;
-    velocidadeBolaX = random(3, 6) * direcaoX;
-    velocidadeBolaY = random(2, 5) * direcaoY;
+    velocidadeBolaX = random(3, 6) * Math.sign(velocidadeBolaX);
+    velocidadeBolaY = random(2, 5) * Math.sign(velocidadeBolaY);
 }
 
 function criarBotoes() {
     if (botoesCriados) return;
     botoesCriados = true;
-    
-    let botaoCima = createButton("⬆");
-    botaoCima.position(50, height - 100);
-    botaoCima.size(50, 50);
-    botaoCima.mousePressed(() => alvoJogadorY -= velocidadeJogador * 10);
-    
-    let botaoBaixo = createButton("⬇");
-    botaoBaixo.position(50, height - 40);
-    botaoBaixo.size(50, 50);
-    botaoBaixo.mousePressed(() => alvoJogadorY += velocidadeJogador * 10);
+    criarBotao("⬆", 50, height - 150, () => alvoJogadorY -= velocidadeJogador * 10);
+    criarBotao("⬇", 50, height - 80, () => alvoJogadorY += velocidadeJogador * 10);
+}
+
+function criarBotao(label, x, y, acao) {
+    let botao = createButton(label);
+    botao.position(x, y);
+    botao.size(60, 60);
+    botao.mousePressed(acao);
 }
